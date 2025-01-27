7dcebc73c2d307a44bf7fa6de9dd8b81615363c2
Umer Farooq <muhammadumerf132@gmail.com>
2025-01-27 14:16:41 +0500

    index

---
+++ inc/header.php
@@ -0,0 +1,5 @@
+<body>
+    <header>
+        <h1>Welcome to My Website</h1>
+        <p>Your go-to place for learning HTML!</p>
+    </header>
---
+++ index.php
@@ -0,0 +1,16 @@
+<?php include('./inc/head.php');?>
+<?php include('./inc/header.php');?>
+
+<main class="container">
+    <div class="bg-dark text-white p-5">
+    <h2>About Me</h2>
+    <p>Hello! I am learning HTML and creating my first web page. It’s fun and exciting!</p>
+    <h2>Contact Me</h2>
+    <ul>
+        <li>Email: example@example.com</li>
+        <li>Phone: 123-456-7890</li>
+    </ul>
+    </div>
+    
+</main>
+<?php include('./inc/footer.php');?>
